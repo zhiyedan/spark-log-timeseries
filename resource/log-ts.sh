1d8618daca8779dbfff08af478b2d3098bc8e1f0
steven <shijia0620@126.com>
2018-04-10 20:18:53 +0800

    add sh

---
+++ resource/log-ts.sh
@@ -0,0 +1,10 @@
+spark-submit \
+--master yarn \
+--driver-memory 8g \
+--num-executors 25 \
+--executor-memory 10g \
+--executor-cores 4 \
+log-timeseries.jar \
+'/user/shijia/netLog2016_ZhongXinTong_20180[2-3][0-3][0-9]_merge.ok' \
+/user/shijia/netResult
+
--- src/LogTimeSeries.scala
+++ src/LogTimeSeries.scala
@@ -6,21 +6,23 @@ import org.apache.spark.{SparkConf, SparkContext}
   */
 object LogTimeSeries {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("log").setMaster("local[*]")
+    val inFile = args(0)
+    val outFile = args(1)
+    val conf = new SparkConf().setAppName("log")
     val sc = new SparkContext(conf)
-    val inputRdd = sc.textFile("/home/zhiyedan/wisetone/wiseNetLog")
+    val inputRdd = sc.textFile(inFile)
+        .filter( x => x.contains("783233"))
       .map(x => {
         val array = x.split("\t")
         val time = array(1).toLong/60
         ((array(19), time*60), 1)
       })
       .reduceByKey(_ + _)
-      //      .reduceByKey({case (l, r) => l + r})
       .sortByKey()
       // 去掉括号
       .map(line => {
       line._1._1 +"\t" + line._1._2 + "\t" + line._2
     })
-      .saveAsTextFile("/home/zhiyedan/wisetone/result.log")
+      .saveAsTextFile(outFile)
   }
 }
