626e9a454afdc883da6446340e1bcf2d8ebeb6bd
steven <shijia0620@126.com>
2018-04-10 21:36:16 +0800

    merge to one file

--- src/LogTimeSeries.scala
+++ src/LogTimeSeries.scala
@@ -6,16 +6,17 @@ import org.apache.spark.{SparkConf, SparkContext}
   */
 object LogTimeSeries {
   def main(args: Array[String]): Unit = {
-    val inFile = args(0)
-    val outFile = args(1)
-    val conf = new SparkConf().setAppName("log")
+    val inFile = "/home/zhiyedan/wisetone/netLog2016_ZhongXinTong_20180310_merge.ok"
+    val outFile = "/home/zhiyedan/wisetone/resultDir"
+    val minute = 1
+    val conf = new SparkConf().setAppName("log").setMaster("local[*]")
     val sc = new SparkContext(conf)
     val inputRdd = sc.textFile(inFile)
         .filter( x => x.contains("783233"))
       .map(x => {
         val array = x.split("\t")
-        val time = array(1).toLong/60
-        ((array(19), time*60), 1)
+        val time = array(1).toLong/60/minute
+        ((array(19), time*60*minute), 1)
       })
       .reduceByKey(_ + _)
       .sortByKey()
@@ -23,6 +24,7 @@ object LogTimeSeries {
       .map(line => {
       line._1._1 +"\t" + line._1._2 + "\t" + line._2
     })
+      .repartition(1) //合成一个文件
       .saveAsTextFile(outFile)
   }
 }
