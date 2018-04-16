1328d66f7c5cc1cb730e73cedd08350b292a42cf
steven <shijia0620@126.com>
2018-04-16 13:11:26 +0800

    filter

--- src/LogTimeSeries.scala
+++ src/LogTimeSeries.scala
@@ -1,3 +1,5 @@
+import java.util.Date
+
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -8,21 +10,37 @@ object LogTimeSeries {
   def main(args: Array[String]): Unit = {
     val inFile = "/home/zhiyedan/wisetone/netLog2016_ZhongXinTong_20180310_merge.ok"
     val outFile = "/home/zhiyedan/wisetone/resultDir"
+
     val minute = 1
+    val now:Long = new Date().getTime / 1000
+    val startTime:Long = 1520611200
+
     val conf = new SparkConf().setAppName("log").setMaster("local[*]")
     val sc = new SparkContext(conf)
+
     val inputRdd = sc.textFile(inFile)
-        .filter( x => x.contains("783233"))
+      .filter(x => x.contains("783233"))
       .map(x => {
         val array = x.split("\t")
-        val time = array(1).toLong/60/minute
-        ((array(19), time*60*minute), 1)
+        if (array.length < 20)
+          (("null",now),0)
+        else {
+          val time:Long = array(1).toLong / 60 / minute
+          ((array(19), time * 60 * minute), 1)
+        }
+      })
+      //过滤时间不符合/mac不符合
+      .filter(x => {
+        if (x._1._1.contains("-") && x._1._2 < now && x._1._2 >= startTime)
+          true
+        else
+          false
       })
       .reduceByKey(_ + _)
       .sortByKey()
       // 去掉括号
       .map(line => {
-      line._1._1 +"\t" + line._1._2 + "\t" + line._2
+      line._1._1 + "\t" + line._1._2 + "\t" + line._2
     })
       .repartition(1) //合成一个文件
       .saveAsTextFile(outFile)
--- src/makeData.scala
+++ src/makeData.scala
@@ -8,18 +8,19 @@ import org.apache.spark.{SparkConf, SparkContext}
   */
 object makeData {
   def main(args: Array[String]): Unit = {
-    val inputFile = "/home/zhiyedan/wisetone/headData.log"
+    val inputFile = "/home/zhiyedan/wisetone/awifi-data/netLog2016_ZhongXinTong_20180310_merge.ok"
+//    val inputFile = "/home/zhiyedan/wisetone/awifi-data/headData.log"
     /*args(0)*/
-    val day = 59
+    val day = 3
     /*args(1).toInt*/
-    val outputFile = "/home/zhiyedan/wisetone/make-data/"
+    val outputFile = "/home/zhiyedan/wisetone/awifi-data/fake-data/"
     /*args(2)*/
     val conf = new SparkConf().setAppName("make-data").setMaster("local[4]")
     val sc = new SparkContext(conf)
     val originRdd = sc.textFile(inputFile).persist()
-    val fileNames = genNames()
+    val fileNames = genNames(day)
     for (i <- 1 to day) {
-      val newData = originRdd.map(line => {
+      val newData = originRdd.filter(line=>line.split("\t").length>1).map(line => {
         val arr = line.split("\t")
         val newTime = arr(1).toLong + i * 60 * 60 * 24
         line.replace(arr(1), newTime.toString)
@@ -30,11 +31,11 @@ object makeData {
 
   }
 
-  def genNames(): Array[String] = {
+  def genNames(days:Int): Array[String] = {
     val array: Array[String] = new Array[String](59)
     val prefix = "netLog2016_ZhongXinTong_20180"
     val postfix = "_merge.ok"
-    for (i <- 0 to 58) {
+    for (i <- 0 to days-1) {
       if (i < 28) {
         val day = i + 1
         if (day < 10)
